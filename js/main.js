2b25bf434da77a1cd83b736fe93945681b4968a9
Benjamin Segal <ben.segal7@gmail.com>
2017-02-20 21:50:30 -0600

    Module 5a Assignment Complete

--- js/main.js
+++ js/main.js
@@ -32,29 +32,83 @@ function onEachFeature(feature, layer) {
 };
 
 //function to retrieve the data and place it on the map
+// function createPropSymbols(map){
+//     //load the data
+//     $.ajax("data/map.geojson", {
+//         dataType: "json",
+//         success: function(response){
+//             //create marker options
+//             var geojsonMarkerOptions = {
+//                 radius: 8,
+//                 fillColor: "#ff7800",
+//                 color: "#000",
+//                 weight: 1,
+//                 opacity: 1,
+//                 fillOpacity: 0.8
+//             };
+
+//             //create a Leaflet GeoJSON layer and add it to the map
+//             L.geoJson(response, {
+//                 pointToLayer: function (feature, latlng){
+//                     return L.circleMarker(latlng, geojsonMarkerOptions);
+//                 }
+//             }).addTo(map);
+//         }
+//     });
+// };
+
+
+function calcPropRadius(attValue) {
+    //scale factor to adjust symbol size evenly
+    var scaleFactor = 30;
+    //area based on attribute value and scale factor
+    var area = attValue * scaleFactor;
+    //radius calculated based on area
+    var radius = Math.sqrt(area/Math.PI);
+
+    return radius;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map){
+    var attribute = "AQI_16";
+    //create marker options
+    var geojsonMarkerOptions = {
+        radius: 8,
+        fillColor: "#ff7800",
+        color: "#000",
+        weight: 1,
+        opacity: 1,
+        fillOpacity: 0.8
+    };
+
+    //create a Leaflet GeoJSON layer and add it to the map
+    L.geoJson(data, {
+        pointToLayer: function (feature, latlng) {
+            //Step 5: For each feature, determine its value for the selected attribute
+            var attValue = Number(feature.properties[attribute]);
+
+            //examine the attribute value to check that it is correct
+            geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+            return L.circleMarker(latlng, geojsonMarkerOptions);
+        }
+    }).addTo(map);
+};
+
+//Step 2: Import GeoJSON data
 function getData(map){
     //load the data
     $.ajax("data/map.geojson", {
         dataType: "json",
         success: function(response){
-            //create marker options
-            var geojsonMarkerOptions = {
-                radius: 8,
-                fillColor: "#ff7800",
-                color: "#000",
-                weight: 1,
-                opacity: 1,
-                fillOpacity: 0.8
-            };
-
-            //create a Leaflet GeoJSON layer and add it to the map
-            L.geoJson(response, {
-                pointToLayer: function (feature, latlng){
-                    return L.circleMarker(latlng, geojsonMarkerOptions);
-                }
-            }).addTo(map);
+            //call function to create proportional symbols
+            createPropSymbols(response, map);
         }
     });
 };
+
+
+
 
 $(document).ready(createMap);
