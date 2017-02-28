3ab90138fdb3696255651fe7a01e75b8b785aa9f
Benjamin Segal <ben.segal7@gmail.com>
2017-02-28 12:47:45 -0600

    Module 5b Complete

--- js/main.js
+++ js/main.js
@@ -31,31 +31,7 @@ function onEachFeature(feature, layer) {
     };
 };
 
-//function to retrieve the data and place it on the map
-// function createPropSymbols(map){
-//     //load the data
-//     $.ajax("data/map.geojson", {
-//         dataType: "json",
-//         success: function(response){
-//             //create marker options
-//             var geojsonMarkerOptions = {
-//                 radius: 8,
-//                 fillColor: "#ff7800",
-//                 color: "#000",
-//                 weight: 1,
-//                 opacity: 1,
-//                 fillOpacity: 0.8
-//             };
 
-//             //create a Leaflet GeoJSON layer and add it to the map
-//             L.geoJson(response, {
-//                 pointToLayer: function (feature, latlng){
-//                     return L.circleMarker(latlng, geojsonMarkerOptions);
-//                 }
-//             }).addTo(map);
-//         }
-//     });
-// };
 
 
 function calcPropRadius(attValue) {
@@ -70,11 +46,39 @@ function calcPropRadius(attValue) {
 };
 
 //Add circle markers for point features to the map
-function createPropSymbols(data, map){
-    var attribute = "AQI_16";
+// function createPropSymbols(data, map){
+//     var attribute = "AQI_16";
+//     //create marker options
+//     var geojsonMarkerOptions = {
+//         fillColor: "#ff7800",
+//         color: "#000",
+//         weight: 1,
+//         opacity: 1,
+//         fillOpacity: 0.8
+//     };
+
+//     //create a Leaflet GeoJSON layer and add it to the map
+//     L.geoJson(data, {
+//         pointToLayer: function (feature, latlng) {
+//             //Step 5: For each feature, determine its value for the selected attribute
+//             var attValue = Number(feature.properties[attribute]);
+
+//             //examine the attribute value to check that it is correct
+//             geojsonMarkerOptions.radius = calcPropRadius(attValue);
+
+//             return L.circleMarker(latlng, geojsonMarkerOptions);
+//         }
+//     }).addTo(map);
+// };
+
+//function to convert markers to circle markers
+function pointToLayer(feature, latlng, attributes){
+    //Determine which attribute to visualize with proportional symbols
+    var attribute = attributes[0];
+    console.log(attribute);
+
     //create marker options
-    var geojsonMarkerOptions = {
-        radius: 8,
+    var options = {
         fillColor: "#ff7800",
         color: "#000",
         weight: 1,
@@ -82,28 +86,171 @@ function createPropSymbols(data, map){
         fillOpacity: 0.8
     };
 
+    //For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
+
+    //Give each feature's circle marker a radius based on its attribute value
+    options.radius = calcPropRadius(attValue);
+
+    //create circle marker layer
+    var layer = L.circleMarker(latlng, options);
+
+    //build popup content string
+    var popupContent = "<p><b>City:</b> " + feature.properties.City;
+
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>Air Quality Index " + "20" + year + ":</b> " + feature.properties[attribute];
+    //bind the popup to the circle marker
+    layer.bindPopup(popupContent);
+
+    //return the circle marker to the L.geoJson pointToLayer option
+    return layer;
+};
+
+//Add circle markers for point features to the map
+function createPropSymbols(data, map, attributes){
     //create a Leaflet GeoJSON layer and add it to the map
     L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            //Step 5: For each feature, determine its value for the selected attribute
-            var attValue = Number(feature.properties[attribute]);
-
-            //examine the attribute value to check that it is correct
-            geojsonMarkerOptions.radius = calcPropRadius(attValue);
-
-            return L.circleMarker(latlng, geojsonMarkerOptions);
+        pointToLayer: function(feature, latlng){
+            return pointToLayer(feature, latlng, attributes);
         }
     }).addTo(map);
 };
 
-//Step 2: Import GeoJSON data
+
+//Step 1: Create new sequence controls
+function createSequenceControls(map, attributes){
+    //create range input element (slider)
+    $('#panel').append('<input class="range-slider" type="range">');
+
+     //set slider attributes
+    $('.range-slider').attr({
+        max: 6,
+        min: 0,
+        value: 0,
+        step: 1
+    });
+    $('#panel').append('<button class="skip" id="reverse">Reverse</button>');
+    $('#panel').append('<button class="skip" id="forward">Skip</button>');
+
+    //Step 5: click listener for buttons
+    $('.skip').click(function(){
+        //get the old index value
+        var index = $('.range-slider').val();
+
+        //Step 6: increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward'){
+            index++;
+            //Step 7: if past the last attribute, wrap around to first attribute
+            index = index > 6 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse'){
+            index--;
+            //Step 7: if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 6 : index;
+        };
+
+        //Step 8: update slider
+        $('.range-slider').val(index);
+        updatePropSymbols(map, attributes[index]);
+    });
+
+    //Step 5: input listener for slider
+    $('.range-slider').on('input', function(){
+        //Step 6: get the new index value
+        var index = $(this).val();  
+        updatePropSymbols(map, attributes[index]);  
+    });
+};
+
+//Step 10: Resize proportional symbols according to new attribute values
+function updatePropSymbols(map, attribute){
+    map.eachLayer(function(layer){
+        if (layer.feature && layer.feature.properties[attribute]){
+            //access feature properties
+            var props = layer.feature.properties;
+
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
+
+            //add city to popup content string
+            var popupContent = "<p><b>City:</b> " + props.City + "</p>";
+
+            //add formatted attribute to panel content string
+            var year = attribute.split("_")[1];
+            popupContent += "<p><b>Air Quality Index " + "20" + year + ":</b> " + layer.feature.properties[attribute];
+
+            //replace the layer popup
+            layer.bindPopup(popupContent, {
+                offset: new L.Point(0,-radius)
+            });
+        };
+    });
+};
+
+function processData(data){
+    //empty array to hold attributes
+    var attributes = [];
+
+    //properties of the first feature in the dataset
+    var properties = data.features[0].properties;
+
+    //push each attribute name into attributes array
+    for (var attribute in properties){
+        //only take attributes with population values
+        if (attribute.indexOf("AQI") > -1){
+            attributes.push(attribute);
+        };
+    };
+
+    //check result
+    console.log(attributes);
+
+    return attributes;
+};
+
+function overlayData(map){
+
+    //var stores the pollution sources
+    var scherer = L.marker([33.06259, -83.80388]).bindPopup('Scherer Power Plant'),
+        alabama = L.marker([33.631013, -87.057016]).bindPopup('Alabama Power Company'),
+        navajo = L.marker([36.903548, -87.767415]).bindPopup('Navajo Power Plant'),
+        gibson = L.marker([38.372031, -87.767415]).bindPopup('Gibson Power Plant'),
+        washington = L.marker([29.484399, -95.62942]).bindPopup('W.A. Parish Power Plant'),
+        rockport = L.marker([37.92739, -87.038363]).bindPopup('Rockport Power Plant'),
+        bowen = L.marker([34.113739, -84.925529]).bindPopup('Bowen Power Plant'),
+        bruce= L.marker([40.630728, -80.42155]).bindPopup('Bruce Mansfield Power Plant'),
+        gavin = L.marker([38.94229, -82.112083]).bindPopup('Gavin Power Plant'),
+        calaveras = L.marker([29.289989, -98.353913]).bindPopup('Calaveras Power Station');
+
+    //converts sources into layer
+    var powerPlants = L.layerGroup([scherer, alabama, navajo, gibson, washington, rockport, bowen, bruce,
+        gavin, calaveras]);
+
+    //provides styling name for layer
+    var overlayMaps = {
+        "Pollution Sources" : powerPlants
+    };
+
+    //adds control to map
+    L.control.layers(null, overlayMaps).addTo(map);
+
+}
+
+//Import GeoJSON data
 function getData(map){
     //load the data
     $.ajax("data/map.geojson", {
         dataType: "json",
         success: function(response){
+
+             //create an attributes array
+            var attributes = processData(response);
+
             //call function to create proportional symbols
-            createPropSymbols(response, map);
+            createPropSymbols(response, map, attributes);
+            createSequenceControls(map, attributes);
+            overlayData(map);
         }
     });
 };
